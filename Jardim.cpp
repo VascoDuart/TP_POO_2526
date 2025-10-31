619e76a709a46a1c4b448e6bb619ca0666f03422
DavidLourenço <davidmlourenco19@gmail.com>
2025-10-31 15:17:25 +0000

    Updated class jardim

--- Jardim.cpp
+++ Jardim.cpp
@@ -1,5 +1,17 @@
-//
-// Created by david on 24/10/2025.
-//
-
 #include "Jardim.h"
+
+Jardim::Jardim(int linhas, int colunas)
+    : numLinhas(linhas), numColunas(colunas)
+{
+    grelha = new Posicao*[numLinhas];
+    for (int i = 0; i < numLinhas; ++i) {
+        grelha[i] = new Posicao[numColunas];
+    }
+}
+
+Jardim::~Jardim() {
+    for (int i = 0; i < numLinhas; ++i) {
+        delete[] grelha[i];
+    }
+    delete[] grelha;
+}
